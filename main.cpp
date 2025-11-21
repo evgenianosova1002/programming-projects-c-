0c44d0e53c53a6e29e9abca4e24c48e85c94acd0
rrlyez <evgenia.nosova1002@gmail.com>
2025-11-22 01:28:06 +0200

    Added lab5

--- main.cpp
+++ main.cpp
@@ -1,75 +1,67 @@
-#include <barrier>
-#include <functional>
 #include <iostream>
 #include <syncstream>
+#include <chrono>
+#include <future>
 #include <thread>
-#include <vector>
+#include <string>
 
-constexpr int kThreadCount = 5;
-constexpr int kSteps = 15;
+using namespace std;
 
-const char kSchedule[kThreadCount][kSteps] = {
-    { 'a', 'a', 'b', 'c', 'c', 'd', 'e', 'f', 'f', 'g', 'h', 'i', 'i', 'j', 'j' },
-    { 'a', 'b', 'b', 'c', 'c', 'd', 'e', 'f', 'g', 'g', 'h', 'i', '-', 'j', 'j' },
-    { 'a', 'b', 'b', 'c', 'c', 'd', 'e', 'f', 'g', 'g', 'h', 'i', '-', 'j', 'j' },
-    { 'a', 'b', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'g', 'h', 'i', '-', 'j', '-' },
-    { 'a', 'b', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'h', 'i', '-', 'j', '-' },
-};
-
-const int kIndexSchedule[kThreadCount][kSteps] = {
-    { 1, 6, 5, 1, 6, 3, 1, 1, 6, 5, 2, 1, 6, 1, 6 },
-    { 2, 1, 6, 2, 7, 4, 2, 2, 1, 6, 3, 2, 0, 2, 7 },
-    { 3, 2, 7, 3, 8, 5, 3, 3, 2, 7, 4, 3, 0, 3, 8 },
-    { 4, 3, 8, 4, 1, 6, 4, 4, 3, 8, 5, 4, 0, 4, 0 },
-    { 5, 4, 9, 5, 2, 7, 5, 5, 4, 1, 6, 5, 0, 5, 0 },
-};
-
-void f(char set_name, int action_index)
-{
-    std::osyncstream out(std::cout);
-    out << "From set " << set_name
-        << " executed action " << action_index << ".\n";
+void perform_task_with_delay(const string& task_name, int duration_seconds) {
+    this_thread::sleep_for(chrono::seconds(duration_seconds));
+    osyncstream(cout) << "Task " << task_name << " completed" << endl;
 }
 
-void worker(int tid, std::barrier<>& sync_point)
-{
-    for (int step = 0; step < kSteps; ++step)
-    {
-        char set_name = kSchedule[tid][step];
-        int  action_index = kIndexSchedule[tid][step];
-
-        if (set_name != '-')
-        {
-            f(set_name, action_index);
-        }
-
-        sync_point.arrive_and_wait();
-    }
+void perform_slow_task(const string& task_name) {
+    perform_task_with_delay(task_name, 7);
 }
 
-int main()
-{
-    {
-        std::osyncstream out(std::cout);
-        out << "Computation started.\n";
-    }
+void perform_fast_task(const string& task_name) {
+    perform_task_with_delay(task_name, 1);
+}
 
-    std::barrier sync_point(kThreadCount);
+void work() {
+    using chrono::steady_clock;
+    using chrono::milliseconds;
+    using chrono::duration_cast;
 
-    {
-        std::vector<std::jthread> threads;
-        threads.reserve(kThreadCount);
+    steady_clock::time_point work_start_time = steady_clock::now();
 
-        for (int tid = 0; tid < kThreadCount; ++tid)
-        {
-            threads.emplace_back(worker, tid, std::ref(sync_point));
-        }
-    }
+    future<void> async_result_A1 = async(launch::async, []() {
+        perform_slow_task("A1");
+        });
 
-    {
-        std::osyncstream out(std::cout);
-        out << "Computation finished.\n";
-    }
+    future<void> async_result_C2 = async(launch::async, []() {
+        perform_slow_task("C2");
+        });
 
+    perform_fast_task("A2");
+    perform_fast_task("C1");
+
+    async_result_A1.wait();
+    async_result_C2.wait();
+
+    
+    future<void> async_result_D2 = async(launch::async, []() {
+        perform_fast_task("D2");
+        });
+
+    perform_fast_task("B");
+
+    perform_fast_task("D1");
+
+    async_result_D2.wait();
+
+    steady_clock::time_point work_finish_time = steady_clock::now();
+
+    double total_execution_seconds =
+        duration_cast<milliseconds>(work_finish_time - work_start_time).count() / 1000.0;
+
+    osyncstream(cout) << "Work is done!" << endl;
+    osyncstream(cout) << "Total time: " << total_execution_seconds << " seconds" << endl;
+}
+
+int main() {
+    work();
     return 0;
-}
+} 
